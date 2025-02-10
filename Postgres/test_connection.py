8d35702c22831a815f9a0eb08bead1211375f36d
Santiago Marin <122939398+Draxord@users.noreply.github.com>
2025-02-10 12:36:45 -0500

    Delete Postgres directory

--- Postgres/test_connection.py
+++
@@ -1,9 +0,0 @@
-from CRUD.database_connection import PostgresDatabaseConnection
-
-if __name__ == "__main__":
-    db = PostgresDatabaseConnection()
-    db.connect()
-    if db.connection:
-        print("La conexión se estableció correctamente.")
-    else:
-        print("No se pudo establecer la conexión.")
