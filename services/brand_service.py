ea89f222f031a5050ca436db2ecf3dd374e558d8
Santiago Marin <122939398+Draxord@users.noreply.github.com>
2025-02-15 05:35:40 -0500

    Update brand_service.py

--- services/brand_service.py
+++ services/brand_service.py
@@ -1,34 +1,35 @@
 from fastapi import APIRouter, HTTPException, status
-from typing import List
-from CRUD.brand import BrandCRUD, BrandCreate, BrandData
+from CRUD.brand import BrandData, BrandCRUD, BrandCreate
 
 router = APIRouter()
 crud = BrandCRUD()
 
-@router.post("/brand/create", response_model=BrandData)
+@router.post("/brand/create", response_model=int)
 def create(data: BrandCreate):
-    return crud.create(data)
-
-@router.get("/brand/get_all", response_model=List[BrandData])
-def get_all(limit: int = 50, offset: int = 0):
-    return crud.get_all(limit, offset)
-
-@router.get("/brand/get/{id_}", response_model=BrandData)
-def get_by_id(id_: int):
-    return crud.get_by_id(id_)
+    brand_id = crud.create(data)
+    if brand_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Error creating brand"
+        )
+    return brand_id
 
 @router.put("/brand/update/{id_}")
-def update(id_: int, data: BrandCreate):
+def update(id_: int, data: BrandData):
     return crud.update(id_, data)
 
 @router.delete("/brand/delete/{id_}")
 def delete(id_: int):
     return crud.delete(id_)
 
-@router.get("/brand/get_by_name/{name}", response_model=List[BrandData])
+@router.get("/brand/get_by_id/{id_}")
+def get_by_id(id_: int):
+    return crud.get_by_id(id_)
+
+@router.get("/brand/get_all")
+def get_all():
+    return crud.get_all()
+
+@router.get("/brand/get_by_name/{name}")
 def get_by_name(name: str):
     return crud.get_by_name(name)
-
-@router.get("/brand/get_by_country/{country}", response_model=List[BrandData])
-def get_by_country(country: str):
-    return crud.get_by_country(country)
